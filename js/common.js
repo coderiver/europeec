d70f76bfc2c64a89adfa5ccc0bb808b7112b8b4e
akella <akella.a@gmail.com>
2014-07-11 16:25:20 +0300

    done

--- js/common.js
+++ js/common.js
@@ -1,3 +1,13 @@
 head.ready(function() {
 	
+
+
+
+	// touch js
+	// uncomment on production
+	//if(head.touch){
+		$('.nav__show').click(function(event) {
+			$('.nav').toggleClass('is-active')
+		});
+	//}
 });
